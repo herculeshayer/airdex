f240cbdc7f556c9227afc8981691e184b027e9b2
Harman Hayer <hhhayer@gmail.com>
2021-11-10 13:15:36 -0800

    init commit+del unncessary files+add sass

---
+++ src/components/GetAQI.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+
+const GetAQIInformation = () => {
+    const [ data, setData ] = useState([]);
+
+    const fetchData = fetch('')
+}
+
+export default GetAQIInformation;
